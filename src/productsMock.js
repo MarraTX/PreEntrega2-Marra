bdd349b947a42733fabcae4ce9ec12e4ce53dcc6
Valentin Marra <marravalentin3@gmail.com>
2024-10-13 15:14:14 -0300

    se hace agrega una branch

---
+++ src/productsMock.js
@@ -0,0 +1,42 @@
+export const products = [
+  {
+    id: "1",
+    title: "Fango",
+    price: 123,
+    stock: 12,
+    description: "Perfecto para tu cuerpo",
+    category: "Fangoterapia",
+    imageUrl:
+      "https://res.cloudinary.com/dpjbuipgk/image/upload/v1728819821/IMG-20240828-WA0014_npptjy.jpg",
+  },
+  {
+    id: "2",
+    title: "Bruma",
+    price: 123,
+    stock: 12,
+    description: "Genial para tu cara",
+    category: "Lifting",
+    imageUrl:
+      "https://res.cloudinary.com/dpjbuipgk/image/upload/v1728819826/IMG-20240828-WA0018_b45ej4.jpg",
+  },
+  {
+    id: "3",
+    title: "Sales",
+    price: 123,
+    stock: 12,
+    description: "Perfecta para baños",
+    category: "Lifitng",
+    imageUrl:
+      "https://res.cloudinary.com/dpjbuipgk/image/upload/v1728819830/IMG-20240828-WA0019_plgq9h.jpg",
+  },
+  {
+    id: "4",
+    title: "Paquetes",
+    price: 123,
+    stock: 12,
+    description: "Que mejor que un paquete",
+    category: "Lifitng",
+    imageUrl:
+      "https://res.cloudinary.com/dpjbuipgk/image/upload/v1728819842/cristales_de_sulfato_qkx3di.png",
+  },
+];
